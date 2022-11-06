9ba852c0b25e9279ba45558bd39675e5ba11dc90
unknown <khatchigmaljiant@gmail.com>
2022-11-06 12:13:35 +0400

    react-js first page

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/header";
+import SECTION01 from "./components/section01";
+import SECTION02 from "./components/sectiom02"
+import Footer from "./components/footer";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <SECTION01></SECTION01>
+      <SECTION02></SECTION02>
+      <Footer></Footer>
+    </>
+
+  );
+}
+
+export default App;
---
+++ src/components/sectiom02/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class SECTION02 extends React.Component{
+
+    render() {
+        return <section className="section2">
+            <h3>More stuff here.</h3>
+            <p>sit malesuada lacus pellus parturpiscing. Pellenterdumat maecenatoque cras a magna nibh et quis diam ames et. Laoremvolutpat ac dolor eget eget temper lacus vestibus velit lacus venean. Magnaipsum tellus morbi leo aliquat nulla convallis pellentesque</p>
+        </section>
+    }
+}
+
+export default SECTION02;
